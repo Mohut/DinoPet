e32d662f723ef6b6a8475ce733bfa6321d0352a0
Ducz01 <tranthinhduc01@web.de>
2021-03-13 18:20:16 +0100

    Minigame2 prototype

--- DinoPet/Assets/Score.cs
+++ DinoPet/Assets/Score.cs
@@ -8,15 +8,35 @@ public class Score : MonoBehaviour
     public int score;
 
     public int health;
+
+    public float timetogetscore;
+
+    public float startscoretime;
+    
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        startscoretime = timetogetscore;
+
     }
 
     // Update is called once per frame
     void Update()
     {
+
+        if (timetogetscore <= 0)
+        {
+            score++;
+            Debug.Log(score);
+            timetogetscore = startscoretime;
+        }
+        else
+        {
+            timetogetscore -= Time.deltaTime;
+        }
+        
+        
         if (health <= 0)
         {
             Debug.Log("Game Over");
